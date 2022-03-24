ad0cd4fe06aa4a55bbc3b8055e73e0c4faf8fbd6
203432 <203432@ids.upchiapas.edu.mx>
2022-03-24 09:53:56 -0600

    Version casi final

--- Flujo/serializers.py
+++ Flujo/serializers.py
@@ -4,8 +4,11 @@ from rest_framework import routers, serializers, viewsets
 #importacion do modelos
 
 from Flujo.models import FlujoEfec
+from categorias.models import Categoria
 
 class FlujoEfecSerializer(serializers.ModelSerializer):
+    categoria = serializers.ReadOnlyField(source = 'id_categoria.categoria')
+    subCategoria = serializers.ReadOnlyField(source = 'id_categoria.subCategoria')
     class Meta:
         model = FlujoEfec
-        fields = ('pk','fecha','mes','descripcion','tipo_flujo','id_categoria', 'cantidad')
+        fields = ('pk','fecha','mes','descripcion','tipo_flujo','id_categoria', 'cantidad','categoria','subCategoria')
